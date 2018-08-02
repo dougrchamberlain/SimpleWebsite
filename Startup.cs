45c052bfb1487fd3df53e57cae124ca5c2ef054e
Doug Chamberlain <d.chamberlain@nextech.com>
2018-08-02 14:09:21 -0400

    updated the message displayed

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace SimpleWebSite
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("QA1 Is live! World!");
             });
         }
     }
